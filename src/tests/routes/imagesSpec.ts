76866c2a13b1d2aa6ac486ec7716200136f54229
hebaahmedsaleh <hebaahmed400@gmail.com>
2022-10-25 19:42:47 +0200

    add last test case

--- src/tests/routes/imagesSpec.ts
+++ src/tests/routes/imagesSpec.ts
@@ -1,11 +1,19 @@
-import request from 'supertest';
-import { app } from '../../index';
+import request from "supertest";
+import { app } from "../../index";
+import sizeOf from "buffer-image-size";
 
-describe('check the server on running and going to images and resize it', (): void => {
-  it('responds with 200 to go to the image not exist in  images folder', async () => {
+describe("check the server on running and going to images and resize it", (): void => {
+  it("check image width and height after resizing", async () => {
+    const width = 400;
+    const height = 500;
     const response = await request(app).get(
-      '/images?filename=icelandwaterfall.jpeg&width=400&height=500'
+      `/images?filename=icelandwaterfall.jpeg&width=${width}&height=${height}`
     );
-    expect(response.status).toEqual(200);
+
+    const dimensions = sizeOf(response.body);
+
+    expect(response.status).toBe(200);
+    expect(dimensions.width).toEqual(width);
+    expect(dimensions.height).toEqual(height);
   });
 });
